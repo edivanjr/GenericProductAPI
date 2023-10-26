b005b7b1e1d246b580341501c45024651dd56d6c
Edivan Jr. <edivan@cliqx.com.br>
2023-10-26 00:04:50 -0300

    First Commit

---
+++ GenericProductAPI.Server/Infrastructure/ProductContext.cs
@@ -0,0 +1,16 @@
+﻿using GenericProductAPI.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace GenericProductAPI.Server.Infrastructure
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ GenericProductAPI.Shared/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProductAPI.Shared
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductDescription { get; set; }
+        public string IsActive { get; set; }
+        public DateTime FabricationDate { get; set; }
+        public DateTime ValidDate { get; set; }
+        public long SupplierCode { get; set; }
+        public string SupplierDescription { get; set; }
+        public long SupplierCNPJ { get; set; }
+    }
+}
